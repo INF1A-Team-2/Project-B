3fcc4d0de393d9dc78a4571286f7521e89531215
ZoutigeWolf <guuskamphuis@gmail.com>
2023-03-13 14:15:11 +0100

    add table id to reservation class

--- RestaurantManager/Reservations/Reservation.cs
+++ RestaurantManager/Reservations/Reservation.cs
@@ -7,15 +7,17 @@ class Reservation
     public TimeOnly Time;
     public int People;
     public MenuType MenuType;
+    public int TableID;
     public string Code;
 
-    public Reservation(int id, DateOnly date, TimeOnly time, int people, MenuType menuType, string code)
+    public Reservation(int id, DateOnly date, TimeOnly time, int people, MenuType menuType, int tableId, string code)
     {
         this.ID = id;
         this.Date = date;
         this.Time = time;
         this.People = people;
         this.MenuType = menuType;
+        this.TableID = tableId;
         this.Code = code;
     }
 }
