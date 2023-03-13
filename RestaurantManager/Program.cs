ea57a511be3ee31c2394e15e7fb1fd273042f5f9
ZoutigeWolf <guuskamphuis@gmail.com>
2023-03-13 14:43:03 +0100

    Add database to program

--- RestaurantManager/Program.cs
+++ RestaurantManager/Program.cs
@@ -2,8 +2,10 @@
 
 static class Program
 {
+    public static readonly DatabaseConnection Database = new DatabaseConnection("DatabaseCredentials.json");
+    
     public static void Main(string[] args)
     {
-
+        
     }
 }
